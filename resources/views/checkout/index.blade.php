08153982f11c26fdf6a5bafdd3c19010835fe133
Any97Cris <criscianysilva1997@gmail.com>
2023-09-22 17:26:24 -0400

    configuracao e estilização com tailwind

---
+++ resources/views/checkout/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <div class="fixed left-0 top-0 hidden lg:block h-full w-1/2 bg-tertiary-900" aria-hidden="true"></div>
+    <div class="fixed right-0 top-0 hidden lg:block h-full w-1/2 bg-tertiary-800" aria-hidden="true"></div>
+
+    <div class="relative mx-outo grid max-w-7xl grid-cols-1 lg:grid-cols-2 gap-x-16 gap-x-8">
+        <section
+        aria-labelledby = "summary-heading"
+        class="bg-tertiary-800 py-12 md:px-10 lg:col-start-2 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:bg-tranparent lg:px-0 lg:pb-24 lg:px-0 lg:pb-24 lg:pt-0"
+        >
+            
+        </section>
+    </div>
+</x-app-layout>
