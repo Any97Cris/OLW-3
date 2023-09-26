8435eb46906366cce7c35c58c029e52e9ecb6cbc
Any97Cris <criscianysilva1997@gmail.com>
2023-09-26 18:36:21 -0400

    components

---
+++ resources/views/components/checkout/summary-item.blade.php
@@ -0,0 +1,7 @@
+<div @class([
+        "boder-t border-white border-opacity-10 pt-6 text-white",
+        "flex items-center justify-between"
+    ])>
+    <dt class="text-primary-200">{{ $title }}</dt>
+    <dt class="text-primary-300">R$ {{ $value }}</dt>
+</div>
