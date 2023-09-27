04f4a52c92972b4717871781680f5be5527958bc
Any97Cris <criscianysilva1997@gmail.com>
2023-09-27 08:30:03 -0400

    configuracao seed order

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Enums\OrderStatusEnum;
+use App\Models\Order;
+use App\Models\Sku;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,15 @@ class OrderSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $cart = Order::factory()
+            ->count(1)
+            ->create([
+                'session_id' => $session_id ?? Str::uuid(),
+                'status' => OrderStatusEnum::CART
+            ]);
+        
+        $cart->each(function($cart){
+            $sku = Sku::with('product')->inRandomOrder()->take(random_int(1,15))->get();
+        });
     }
 }
